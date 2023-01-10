116fc1d16f5c675bfb90d6b2c66e06413ff0240c
Bob-SpringMtTech <RCollins@SpringMountTech.com>
2023-01-10 13:40:39 -0500

    Added nore geometries

--- ValveSeats.py
+++ ValveSeats.py
@@ -1,9 +1,7 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import math
 
-from Library import unit_of_measure as um
-from Library import NIST330 as un
-from Library import Water as h2o
+from Engineering import unit_of_measure as um
+from Engineering import NIST330 as un
 
 def uFormat(q, u, f):
     if(len(u.Symbol) > 0):
@@ -13,105 +11,130 @@ def uFormat(q, u, f):
 
     return str
 
-def poly(x, coeff):
-    y = 0.0
-    for i in range(len(coeff)-1):
-        y = (coeff[i] + y) * x
-    y += coeff[len(coeff)-1]
+# Spherical radius plug against a beveled seat.  All angles are taken from the axis of the parts (half angles)
+class SphPlug_BevelSeat:
+    def __init__(self, diaSeat, angSeat, htSeat, srPlug, htPlug, diaStem):
+        self._diaSeat = diaSeat.SIValue
+        self._angSeat = angSeat.SIValue
+        self._htSeat = htSeat.SIValue
+        self._srPlug = srPlug.SIValue
+        self._htPlug = htPlug.SIValue
+        self._diaStem = diaStem.SIValue
 
-    return y
+        # x1, y1 correspond to the lower corner of the seat
+        self._x1 = self._diaSeat / 2.0             
+        self._y1 = 0                                
 
-# Calculate the flow area between a spherical radius plug and beveled seat.
-# All angles are taken from the axis of the part (half angles)
-def FlowArea_SphPlug_BevSeat(diaSeat, angSeat, htSeat, srPlug, htPlug, diaStem, stroke):
-    angSeatRad = angSeat.SIValue
+        # x2, y2 correspond to the upper corner of the seat
+        self._x2 = self._x1 + self._htSeat * math.tan(self._angSeat)
+        self._y2 = self._y1 + self._htSeat
 
-    # x1, y1 correspond to the lower corner of the seat
-    x1 = diaSeat.SIValue / 2.0             
-    y1 = 0                                
+        # xc, yc correspond to the contact point between the seat and plug (ideal)
+        self._xc = self._srPlug * math.sin(math.pi / 2.0 - self._angSeat)
+        self._yc = self._y1 + (self._xc - self._x1) / math.tan(self._angSeat)
 
-    # x2, y2 correspond to the upper corner of the seat
-    x2 = x1 + htSeat.SIValue * np.tan(angSeatRad)
-    y2 = y1 + htSeat.SIValue
+        # x0, y0 correspond to the center of the spherical radius
+        self._x0 = 0
+        self._y0closed = self._srPlug * math.sin(self._angSeat) + self._yc
 
-    # xc, yc correspond to the contact point between the seat and plug (ideal)
-    xc = srPlug.SIValue * np.sin(np.pi / 2.0 - angSeatRad)
-    yc = y1 + (xc - x1) / np.tan(angSeatRad)
-
-    # x0, y0 correspond to the center of the spherical radius
-    x0 = 0
-    y0closed = srPlug.SIValue * np.sin(angSeatRad) + yc
-
-    # x3, y3 correspond to the lower corner of the plug where the spherical radius ends
-    x3 = np.sqrt(srPlug.SIValue ** 2 - htPlug.SIValue ** 2)
-    y3closed = y0closed - htPlug.SIValue
-
-    y0 = y0closed + stroke.SIValue
-    y3 = y3closed + stroke.SIValue
-
-    angCS = np.arctan((y0 - y2) / x2)
-    angCP = np.arctan((y3 - y2) / (x2 - x3))
-    if (y3 < y2):
-        angCP = 0
+        # x3, y3 correspond to the lower corner of the plug where the spherical radius ends
+        self._x3 = math.sqrt(self._srPlug ** 2 - self._htPlug ** 2)
+        self._y3closed = self._y0closed - self._htPlug
     
-    angFlow = max(angSeatRad, angCS, angCP)
+    @property
+    def ContactDiameter(self):
+        return self._xc * 2.0 * un.Length.m
 
-    if (angFlow == angSeatRad):
-        gap = stroke.SIValue * np.sin(angSeatRad)
-        xp = xc
-        xs = xp + gap * np.cos(angSeatRad)
-        lCone = srPlug.SIValue
-    elif (angFlow == angCS):
-        gap = np.sqrt(((x2 - x0) ** 2) + ((y2 - y0) ** 2)) - srPlug.SIValue
-        xs = x2
-        xp = xs - gap * np.cos(angFlow)
-        lCone = srPlug.SIValue
-    elif (angFlow == angCP):
-        gap = np.sqrt(((x2 - x3) ** 2) + ((y2 - y3) ** 2))
-        xs = x2
-        xp = x3
-        lCone = xp / np.cos(angFlow)
-    else:
-        gap = 0.0
+    # Calculate the flow area for a given stroke.
+    def FlowArea(self, stroke):
+        strokeSI = stroke.SIValue
+        self._y0 = self._y0closed + strokeSI
+        self._y3 = self._y3closed + strokeSI
 
-    areaTruncCone = np.pi * (lCone * (xs - xp) + xs * gap)
+        angCS = math.atan((self._y0 - self._y2) / self._x2)
+        angCP = math.atan((self._y3 - self._y2) / (self._x2 - self._x3))
+        if (self._y3 < self._y2):
+            angCP = 0
+        
+        angFlow = max(self._angSeat, angCS, angCP)
 
-    areaAnnular = np.pi / 4.0 * (diaSeat.SIValue ** 2 - diaStem.SIValue ** 2)
+        if (angFlow == self._angSeat):
+            gap = strokeSI * math.sin(self._angSeat)
+            xp = self._xc
+            xs = xp + gap * math.cos(self._angSeat)
+            lCone = self._srPlug
+        elif (angFlow == angCS):
+            gap = math.sqrt(((self._x2 - self._x0) ** 2) + ((self._y2 - self._y0) ** 2)) - self._srPlug
+            xs = self._x2
+            xp = xs - gap * math.cos(angFlow)
+            lCone = self._srPlug
+        elif (angFlow == angCP):
+            gap = math.sqrt(((self._x2 - self._x3) ** 2) + ((self._y2 - self._y3) ** 2))
+            xs = self._x2
+            xp = self._x3
+            lCone = xp / math.cos(angFlow)
+        else:
+            gap = 0.0
 
-    area = min(areaTruncCone, areaAnnular) * un.Area.meterSq
+        areaTruncCone = math.pi * (lCone * (xs - xp) + xs * gap)
 
-    return area
+        areaAnnular = math.pi / 4.0 * (self._diaSeat ** 2 - self._diaStem ** 2)
 
+        area = min(areaTruncCone, areaAnnular) * un.Area.meterSq
 
-# Calculate the flow area between a ball plug and beveled seat.
-# All angles are taken from the axis of the part (half angles)
-def FlowArea_BallPlug_BevSeat(diaSeat, angSeat, htSeat, diaPlug, stroke):
-    srPlug = diaPlug / 2.0
-    htPlug = srPlug
-    diaStem = 0.0 * uin  
-    return FlowArea_SphPlug_BevSeat(diaSeat, angSeat, htSeat, srPlug, htPlug, diaStem, stroke)
+        return area
 
 
-# Calculate the flow area between a ball plug and square seat.
-# All angles are taken from the axis of the part (half angles)
-def FlowArea_BallPlug_SqSeat(diaSeat, angSeat, diaPlug, stroke):
-    xs = diaSeat.SIValue / 2.0
-    sr = diaPlug.SIValue / 2.0
-    y0closed = np.sqrt(sr ** 2 - xs ** 2)
-    y0open = y0closed + stroke.SIValue
+# Ball plug against a beveled seat.  All angles are taken from the axis of the parts (half angles)
+class BallPlug_BevSeat:
+    def __init__(self, diaSeat, angSeat, htSeat, diaBall):
+        srPlug = diaBall / 2.0
+        htPlug = srPlug
+        diaStem = 0.0 * un.Length.m
+        self._vlv = SphPlug_BevelSeat(diaSeat, angSeat, htSeat, srPlug, diaStem)
 
-    hypot = np.sqrt(y0open ** 2 + xs ** 2)
-    gap = hypot - sr
+    @property
+    def ContactDia(self):
+        return self._vlv.ContactDiameter 
 
-    xp = xs * sr / hypot
+    # Calculate the flow area for a given stroke.
+    def FlowArea(self, stroke):
+        return self._vlv.FlowArea(stroke)
 
-    areaTruncCone = np.pi * (sr * (xs - xp) + xs * gap)
 
-    areaAnnular = np.pi * xs ** 2
+# Ball plug against a square seat.  All angles are taken from the axis of the parts (half angles)
+class BallPlug_SqSeat:
+    def __init__(self, diaSeat, diaBall):
+        angSeat = 90.0 * un.Angle.deg
+        htSeat = 0.0 * un.Length.m
+        srPlug = diaBall / 2.0
+        diaStem = 0.0 * un.Length.m
+        htPlug = srPlug
+        self._vlv = SphPlug_BevelSeat(diaSeat, angSeat, htSeat, srPlug, htPlug, diaStem)
 
-    area = min(areaTruncCone, areaAnnular) * un.Area.meterSq
+    @property
+    def ContactDia(self):
+        return self._vlv._diaSeat
 
-    return area
+    # Calculate the flow area for a given stroke.
+    def FlowArea(self, stroke):
+        strokeSI = stroke.SIValue
+        y0 = self._vlv._y0closed + strokeSI
+
+        angFlow = math.atan((self._vlv._y0 - self._vlv._y1) / self._vlv._x1)
+
+        gap = math.sqrt(((self._vlv._x1 - self._vlv._x0) ** 2) + ((self._vlv._y1 - y0) ** 2)) - self._vlv._srPlug
+        xs = self._vlv._x1
+        xp = xs - gap * math.cos(angFlow)
+        lCone = self._vlv._srPlug
+
+        areaTruncCone = math.pi * (lCone * (xs - xp) + xs * gap)
+
+        areaAnnular = math.pi / 4.0 * (self._vlv._diaSeat ** 2 - self._vlv._diaStem ** 2)
+
+        area = min(areaTruncCone, areaAnnular) * un.Area.meterSq
+
+        return area
 
 
 # Calculate the flow area between a conical plug and square seat.
@@ -122,48 +145,26 @@ def FlowArea_ConePlug_SqSeat(diaSeat, diaPlugTip, angPlug, stroke):
     # x1, y1 correspond to the corner of the seat
     x1 = diaSeat.SIValue / 2.0             
     y1 = 0                                
-
+2
     # x3, y3 correspond to the lower corner of the plug
     x3 = diaPlugTip.SIValue / 2.0
-    y3 = - (x1 - x3) / np.tan(angPlugRad) + stroke.SIValue
+    y3 = - (x1 - x3) / math.tan(angPlugRad) + stroke.SIValue
 
     xs = x1
-    gap = stroke.SIValue * np.sin(angPlugRad)
-    xp = xs - gap * np.cos(angPlugRad)
+    gap = stroke.SIValue * math.sin(angPlugRad)
+    xp = xs - gap * math.cos(angPlugRad)
     if (xp > x3): # cone is limiting the flow area
-        lCone = xp / np.cos(angPlugRad)
+        lCone = xp / math.cos(angPlugRad)
     else: # cone is fully retracted from the seat
         xp = x3
-        gap = np.sqrt(((x1 - x3) ** 2) + ((y1 - y3) ** 2))
+        gap = math.sqrt(((x1 - x3) ** 2) + ((y1 - y3) ** 2))
         lCone = x3 * gap / (x1 - x3)
 
-    areaTruncCone = np.pi * (lCone * (xs - xp) + xs * gap)
+    areaTruncCone = math.pi * (lCone * (xs - xp) + xs * gap)
 
-    areaAnnular = np.pi / 4.0 * diaSeat.SIValue
+    areaAnnular = math.pi / 4.0 * diaSeat.SIValue
 
     area = min(areaTruncCone, areaAnnular) * un.Area.meterSq
    
     return area
-
-
-utf = un.Temperature.degF
-upsig = un.Pressure.psig
-upsia = un.Pressure.psia
-upsi = un.Pressure.psi
-ulbf = un.Force.lbf
-uin = un.Length.inch
-usqin = un.Area.inSq
-udeg = un.Angle.deg
-
-area = FlowArea_BallPlug_BevSeat(1.000 * uin, 30.0 * udeg, 0.040 * uin, 1.18136721 * uin, 0.125 * uin )
-print(f'BB flow area= {uFormat(area, usqin, "0.5f")}')
-
-area = FlowArea_BallPlug_SqSeat(1.000 * uin, 30.0 * udeg, 1.18136721 * uin, 0.25084678 * uin )
-print(f'BS flow area= {uFormat(area, usqin, "0.5f")}')
-
-area = FlowArea_SphPlug_BevSeat(1.500 * uin, 30.0 * udeg, 0.040 * uin, 0.875 * uin, 0.5625 * uin, 0.3125 * uin, 0.525 * uin)
-print(f'SB flow area= {uFormat(area, usqin, "0.5f")}')
-
-area = FlowArea_ConePlug_SqSeat(0.375 * uin, 0.250 * uin, 30.0 * udeg, 0.200 * uin)
-print(f'CS flow area = {uFormat(area, usqin, "0.5f")}')
 
