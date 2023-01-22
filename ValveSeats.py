67be59c8915c90b25f31bd38e29ac5b1c12eec57
Bob-SpringMtTech <RCollins@SpringMountTech.com>
2023-01-22 15:45:48 -0500

    Implemented conical plug + beveled seat

--- ValveSeats.py
+++ ValveSeats.py
@@ -11,6 +11,245 @@ def uFormat(q, u, f):
 
     return str
 
+conv_fac = 39.37 # convert meters to inches
+
+class Point2d:
+    def __init__(self, x, y, name = None):
+        self.x = x
+        self.y = y
+        self.name = name
+
+    def __str__(self) -> str:
+        return f'({self.name}: {self.x * conv_fac}, {self.y * conv_fac})'
+
+    @property
+    def Name(self):
+        return self.name
+
+    @classmethod
+    def Distance(cls, p1, p2):
+        dx = p2.x - p1.x
+        dy = p2.y - p1.y
+        return math.sqrt(dx ** 2 + dy ** 2)
+
+    def Shift(self, dx = math.nan, dy = math.nan):
+        goodData = (0 if math.isnan(dx) else 2) + (0 if math.isnan(dy) else 1)
+        if (goodData == 0):
+            raise Exception("must specify either 'dx' or 'dy'")
+        elif (goodData == 1): # dy was specified, dx was not
+            self.y += dy
+        elif (goodData == 2): # dx was specified, dy was not
+            self.x += dx
+        else: # both x1 and y1 were specified
+            raise Exception("must not specify both 'dx' and 'dy'")
+        
+        return self
+
+class Line2d:
+    def __init__(self, m, b, name = None):
+        self.m = m
+        self.b = b
+        self.name = name
+
+    def __str__(self) -> str:
+        if (self.b >= 0.0):
+            _b = self.b
+            _sgn = '+'
+        else:
+            _b = -self.b
+            _sgn = '-'
+
+        return f'{self.name}: y = {self.m}x {_sgn} {_b * conv_fac}'
+
+
+    def X(self, y):
+        return (y - self.b) / self.m
+
+    def Y(self, x):
+        return self.m * x + self.b
+
+    @property
+    def Slope(self):
+        return self.m
+
+    @property
+    def Y_Int(self):
+        return self.b
+
+    @property
+    def Name(self):
+        return self.name
+
+    @classmethod
+    def SlopeYInt(cls, m, b, name = None):
+        return Line2d(m, b, name)
+        
+    @classmethod
+    def ThruPointWithSlope(cls, pt, m, name = None):
+        return Line2d(m, pt.y - m * pt.x, name)
+        
+    @classmethod
+    def Thru2Points(cls, p1, p2, name = None):
+        m = (p2.y - p1.y) / (p2.x - p1.x)
+        b = p1.y - m * p1.x
+        return Line2d(m, b, name)
+
+    def Perpendicular(self, thruPoint2d, name = None):
+        m_new = -1.0/self.m
+        b_new = thruPoint2d.y - m_new * thruPoint2d.x
+        return Line2d(m_new, b_new, name)
+
+    def Intersection(self, otherLine, name = None):
+        x_int = (otherLine.b - self.b) / (self.m - otherLine.m)
+        y_int = self.Y(x_int)
+        return Point2d(x_int, y_int, name)
+
+    def Shift(self, dx = math.nan, dy = math.nan):
+        goodData = (0 if math.isnan(dx) else 2) + (0 if math.isnan(dy) else 1)
+        if (goodData == 0):
+            raise Exception("must specify either 'dx' or 'dy'")
+        elif (goodData == 1): # dy was specified, dx was not
+            self.b += dy
+        elif (goodData == 2): # dx was specified, dy was not
+            self.b += self.Y(dx)
+        else: # both x1 and y1 were specified
+            raise Exception("must not specify both 'dx' and 'dy'")
+        
+        return self
+
+class LineSegment2d:
+    def __init__(self, p1, p2, name = None):
+        self.line = Line2d.Thru2Points(p1, p2, name)
+        self.p1 = p1
+        self.p2 = p2
+
+    def __str__(self) -> str:
+        return f'{self.line} from {self.p1} to {self.p2}'
+
+    @classmethod
+    def WithCoordinates(cls, line, x1 = math.nan, y1 = math.nan, x2 = math.nan, y2 = math.nan, name = None):
+        goodData = (0 if math.isnan(x1) else 2) + (0 if math.isnan(y1) else 1)
+        if (goodData == 0):
+            raise Exception("must specify either 'x' or 'y' of point 1")
+        elif (goodData == 1): # y1 was specified, x1 was not
+            x1 = line.X(y1)
+        elif (goodData == 2): # x1 was specified, y1 was not
+            y1 = line.Y(x1)
+        else: # both x1 and y1 were specified
+            raise Exception("must not specify both 'x' and 'y' of point 1")
+    
+        goodData = (0 if math.isnan(x2) else 2) + (0 if math.isnan(y2) else 1)
+        if (goodData == 0):
+            raise Exception("must specify either 'x' or 'y' of point 2")
+        elif (goodData == 1): # y2 was specified, x2 was not
+            x2 = line.X(y2)
+        elif (goodData == 2): # x2 was specified, y2 was not
+            y2 = line.Y(x2)
+        else: # both x2 and y2 were specified
+            raise Exception("must not specify both 'x' and 'y' of point 2")
+
+        return LineSegment2d(Point2d(x1, y1), Point2d(x2, y2), name)
+        
+    @property
+    def Line(self):
+        return self.line
+    
+    @property
+    def Point1(self):
+        return self.p1
+    
+    @property
+    def Point2(self):
+        return self.p2
+    
+    @property
+    def MidPoint(self):
+        midX = (self.p1.x + self.p2.x) / 2.0
+        midY = (self.p1.y + self.p2.y) / 2.0
+        return Point2d(midX, midY)
+
+    @property
+    def Length(self):
+        return Point2d.Distance(self.p1, self.p2)
+
+    def IsWithin(self, pt): # assumes that pt lies on the line defined by the line segment
+        mid = self.MidPoint
+        dist = Point2d.Distance(pt, mid)
+        if (dist < self.Length / 2.0):
+            return True
+        else:
+            return False
+
+    def Shift(self, *, dx = math.nan, dy = math.nan):
+        self.line.Shift(dx, dy)
+        self.p1.Shift(dx, dy)
+        self.p2.Shift(dx, dy)
+        
+        return self
+
+    @property
+    def Name(self):
+        return self.Line.Name
+
+    # calculate the surface area of a truncated cone assuming the axis of rotation is along the y axis
+    def ConicalArea(self):
+        area = math.pi * self.Length * (self.p1.x + self.p2.x)
+        return area
+
+
+class TwoCones:
+    def __init__(self, segmentA, segmentB):
+        self.A = segmentA
+        self.B = segmentB
+        area = math.nan
+        zone = None
+
+    def _CalcArea(self, segment):
+        newArea = segment.ConicalArea()
+        if (newArea < self.area):
+            self.area = newArea
+            self.zone = segment.Name
+
+    def MinArea(self):
+        # start by trying all of the conical sections defined by the end points of both line segments
+        A1_B1 = LineSegment2d(self.A.p1, self.B.p1, f'{self.A.p1.Name} - {self.B.p1.Name}')
+        self.area = A1_B1.ConicalArea()
+        self.zone = A1_B1.Name
+        A1_B2 = LineSegment2d(self.A.p1, self.B.p2, f'{self.A.p1.Name} - {self.B.p2.Name}')
+        self._CalcArea(A1_B2)
+        A2_B1 = LineSegment2d(self.A.p2, self.B.p1, f'{self.A.p2.Name} - {self.B.p1.Name}')
+        self._CalcArea(A2_B1)
+        A2_B2 = LineSegment2d(self.A.p2, self.B.p2, f'{self.A.p2.Name} - {self.B.p2.Name}')
+        self._CalcArea(A2_B2)
+
+        # next, look at the line segments going thru one of the endpoints and perpendicular to the other line segment
+        # point defined by the interesction of a line perpendicular to A passing thru B at pt1 and line A
+        ptA_B1 = self.A.Line.Perpendicular(self.B.p1).Intersection(self.A.Line)
+        perpA_thruB1 = LineSegment2d(self.B.p1, ptA_B1, f'{self.A.Name} - {self.B.p1.Name}')
+        if (self.A.IsWithin(ptA_B1)):
+            self._CalcArea(perpA_thruB1)
+
+        # point defined by the interesction of a line perpendicular to A passing thru B at pt2 and line A
+        ptA_B2 = self.A.Line.Perpendicular(self.B.p2).Intersection(self.A.Line)
+        perpA_thruB2 = LineSegment2d(self.B.p2, ptA_B2, f'{self.A.Name} - {self.B.p2.Name}')
+        if (self.A.IsWithin(ptA_B2)):
+            self._CalcArea(perpA_thruB2)
+
+        # point defined by the interesction of a line perpendicular to B passing thru A at pt1 and line B
+        ptB_A1 = self.B.Line.Perpendicular(self.A.p1).Intersection(self.B.Line) 
+        perpB_thruA1 = LineSegment2d(self.A.p1, ptB_A1, f'{self.B.Name} - {self.A.p1.Name}')
+        if (self.B.IsWithin(ptB_A1)):
+            self._CalcArea(perpB_thruA1)
+
+        # point defined by the interesction of a line perpendicular to B passing thru A at pt2 and line B
+        ptB_A2 = self.B.Line.Perpendicular(self.A.p2).Intersection(self.B.Line) 
+        perpB_thruA2 = LineSegment2d(self.A.p2, ptB_A2, f'{self.B.Name} - {self.A.p2.Name}')
+        if (self.B.IsWithin(ptB_A2)):
+            self._CalcArea(perpB_thruA2)
+
+        return self.area, self.zone
+   
+
 # Calculate the surface area of a truncated cone excluding the circular areas.  Height is the perpendicular
 # distance between the big and small circles (not slant height).  
 # Works for non-truncated cones by setting smallRadius to zero.
@@ -179,7 +418,7 @@ class BallPlug_BeveledSeat:
             htPlug = srPlug
         diaFlatPlug = 0.0 * un.Length.m
         diaStem = 0.0 * un.Length.m
-        self._vlv = SphPlug_BevelSeat(diaSeat, angSeat, htSeat, srPlug, diaFlatPlug, diaStem)
+        self._vlv = SphericalPlug_BeveledSeat(diaSeat, angSeat, htSeat, srPlug, diaFlatPlug, diaStem)
 
     @property
     def SphRadPlug(self):
@@ -281,6 +520,70 @@ class ConicalPlug_SquareSeat:
         self._diaPlugTip = diaPlugTip.SIValue
         self._angPlug = angPlug.SIValue
 
+        self._seat = Point2d(self._diaSeat / 2.0, 0, 'seat corner')
+
+        self._xc = self._diaSeat / 2.0
+        self._yc = 0.0
+
+        plugContact = Point2d(self._xc, self._yc, 'plug contact')
+        plug_line = Line2d.ThruPointWithSlope(plugContact, math.tan(math.pi / 2 - self._angPlug))
+        cone_body_rad = self._xc * 100.0 # set the cone body diameter so large that it will never govern the flow area
+        self._plug_segment = LineSegment2d.WithCoordinates(plug_line, x1 = self._diaPlugTip / 2.0, x2 = cone_body_rad, name='plug face')
+        self._plug_segment.p1.name = 'plug tip'
+
+        self._area_annular = math.pi / 4.0 * self._diaSeat ** 2
+        self._area_zone = 'invalid'
+        
+    def FlowArea(self, stroke):
+        _stroke = stroke.SIValue
+
+        self._plug_segment.Shift(dy = _stroke)
+
+        # start by calculating the tip to corner conical area
+        cornerSegment = LineSegment2d(self._plug_segment.p1, self._seat, f'{self._plug_segment.p1.Name} - {self._seat.Name}')
+        area = cornerSegment.ConicalArea()
+        self._area_zone = cornerSegment.Name
+
+        # next, look at the line segments going thru the seat corner perpendicular to the cone segment
+        normalPt = self._plug_segment.Line.Perpendicular(self._seat).Intersection(self._plug_segment.Line)
+        normalSegment = LineSegment2d(normalPt, self._seat, f'{self._plug_segment.Name} - {self._seat.Name}')
+        if (self._plug_segment.IsWithin(normalPt)):
+            normalArea = normalSegment.ConicalArea()
+            if (normalArea < area):
+                area = normalArea
+                self._area_zone = normalSegment.Name
+
+        if (self._area_annular < area):
+            area = self._area_annular
+            self._area_zone = 'seat diameter'
+    
+        return area * un.Area.meterSq
+
+    @property
+    def ContactDia(self):
+        return self._diaSeat * un.Length.inch 
+
+    @property
+    def ContactLocation(self):
+        return 'seat corner'
+
+    @property
+    def AreaZone(self):
+        return self._area_zone
+
+    @property
+    def MaxArea(self):
+        return self._area_annular * un.Area.meterSq
+
+
+# Calculate the flow area between a conical plug and square seat.
+# All angles are taken from the axis of the part (half angles)
+class ___OLD___ConicalPlug_SquareSeat:
+    def __init__(self, diaSeat, diaPlugTip, angPlug):
+        self._diaSeat = diaSeat.SIValue
+        self._diaPlugTip = diaPlugTip.SIValue
+        self._angPlug = angPlug.SIValue
+
         # x1, y1 correspond to the corner of the seat
         self._x1 = self._diaSeat / 2.0             
         self._y1 = 0                                
@@ -347,6 +650,10 @@ class ConicalPlug_BeveledSeat:
         self._x2 = self._x1 + self._htSeat * math.tan(self._angSeat)
         self._y2 = self._y1 + self._htSeat
 
+        seatLowerCorner = Point2d(self._x1, self._y1, 'seat lower corner')
+        seatUpperCorner = Point2d(self._x2, self._y2, 'seat upper corner')
+        self._seat_segment = LineSegment2d(seatLowerCorner, seatUpperCorner, name='seat face')
+
         if (self._angPlug > self._angSeat): # contact is at the upper corner of the seat.
             self._xc = self._x2
             self._yc = self._y2
@@ -356,60 +663,23 @@ class ConicalPlug_BeveledSeat:
             self._yc = self._y1
             self._contact_location = 'lower corner'
 
+        plugContact = Point2d(self._xc, self._yc, 'plug contact')
+        plug_line = Line2d.ThruPointWithSlope(plugContact, math.tan(math.pi / 2 - self._angPlug))
+        cone_body_rad = self._x2 * 100.0 # set the cone body diameter so large that it will never govern the flow area
+        self._plug_segment = LineSegment2d.WithCoordinates(plug_line, x1 = self._diaPlugTip / 2.0, x2 = cone_body_rad, name='plug face')
+        self._plug_segment.p1.name = 'plug tip'
+
         self._area_annular = math.pi / 4.0 * self._diaSeat ** 2
-    
+        self._area_zone = 'invalid'
         
     def FlowArea(self, stroke):
         _stroke = stroke.SIValue
-        # x3, y3 correspond to the tip of the plug
-        x3 = self._diaPlugTip / 2.0
-        y3 = self._yc + _stroke - (self._xc - x3) / math.tan(self._angPlug)
+        vlv = TwoCones(self._seat_segment, self._plug_segment.Shift(dy = _stroke))
+        area, self._area_zone = vlv.MinArea()
 
-        # calculate area considering only lower seat corner
-        xs = self._x1
-        gap = _stroke * math.sin(self._angPlug)
-        xp = xs - gap * math.cos(self._angPlug)
-        area_zone_1 = 'plug surface to lower corner'
-        if (xp < x3): # cone is retracted past lower seat corner
-            xp = x3
-            gap = math.sqrt(((self._x1 - x3) ** 2) + ((self._y1 - y3) ** 2))
-            area_zone_1 = 'plug tip to lower seat corner'
-            # print (stroke_inch)
-        areaTruncCone_1 = math.pi * gap * (xs + xp)
-
-        # gap between plug tip and perpendicular to seat
-        dist = math.sqrt((x3 - self._x1) ** 2.0 + (y3 - self._y1) ** 2.0) # distance between plug tip and lower seat corner
-        gamma = math.atan((y3 - self._y1) / (x3 - self._x1))
-        beta = self._angSeat - gamma
-        gap = dist * math.cos(beta)
-        xs = x3 + gap * math.cos(self._angSeat)
-        xp = x3
-        ht = gap * math.sin(self._angSeat)
-        areaTruncCone_3 = TruncConeArea(xp, xs, ht)
-        area_zone_3 = 'plug corner to seat surface'
-
-        # calculate area considering only upper seat corner
-        xs = self._x2
-        xp = xs - gap * math.cos(self._angPlug)
-        area_zone_2 = 'plug surface to upper seat corner'
-        if (xp < x3): # cone is retracted past upper seat corner
-            xp = x3
-            gap = math.sqrt(((self._x2 - x3) ** 2) + ((self._y2 - y3) ** 2))
-            area_zone_2 = 'plug tip to upper seat corner'
-            # print (stroke_inch)
-        areaTruncCone_2 = math.pi * gap * (xs + xp)
-
-        area = min(areaTruncCone_1, areaTruncCone_2, areaTruncCone_3, self._area_annular)
-
-        if (area == areaTruncCone_1):
-            self._area_zone = area_zone_1
-        elif (area == areaTruncCone_2):
-            self._area_zone = area_zone_2
-        elif (area == areaTruncCone_3):
-            self._area_zone = area_zone_3
-        elif (area == self._area_annular):
-            self._area_zone = 'seat diameter'
-    
+        if (self._area_annular < area):
+            area = self._area_annular
+            self._area_zone = 'seat bore'
         return area * un.Area.meterSq
 
     @property
